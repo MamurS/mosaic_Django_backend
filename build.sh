87f3c16cdebef9ddce956948b74bfa0789c56ef2
Mamur <40205003+MamurS@users.noreply.github.com>
2025-08-04 06:30:37 +0000

    fix: Use production requirements file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 cd insurance_project
-pip install -r requirements.txt
+pip install -r requirements-prod.txt
 python manage.py collectstatic --no-input
 python manage.py migrate

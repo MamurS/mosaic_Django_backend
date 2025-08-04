c52bf7cde20f3d6d791b56284570325fc65bf8d3
Mamur <40205003+MamurS@users.noreply.github.com>
2025-08-04 06:37:41 +0000

    Update requirements-prud.txt and build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
-cd insurance_project
+
+# Install production dependencies from the root directory
 pip install -r requirements-prod.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+
+# Run Django commands by specifying the path to manage.py
+python insurance_project/manage.py collectstatic --no-input
+python insurance_project/manage.py migrate
